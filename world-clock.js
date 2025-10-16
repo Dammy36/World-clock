40ce6897461d3f106d80d7835ae731878d3dced8
Oseyemi Damilola <osedee101@gmail.com>
2025-10-16 08:52:45 +0100

    Update the Js by added muiltple locations

--- world-clock.js
+++ world-clock.js
@@ -1,25 +1,61 @@
 function upDated() {
-  const nigeriaDate = document.getElementById("nigeria-date");
-  const nigeriaTime = document.getElementById("nigeria-time");
+  const nigeriaCity = document.getElementById("nigeria-city");
+  if (nigeriaCity) {
+    const nigeriaDate = document.getElementById("nigeria-date");
 
-  const currentTime = moment().tz("Africa/Lagos");
-  nigeriaDate.innerHTML = currentTime.format("ddd MMM d YYYY");
-  nigeriaTime.innerHTML = currentTime.format("h:m:s [<small>]A[</small>]");
+    const nigeriaTime = document.getElementById("nigeria-time");
 
-  const losAngelesDate = document.getElementById("los-angeles-date");
-  const losAngelesTime = document.getElementById("los-angeles-time");
+    const currentTime = moment().tz("Africa/Lagos");
+    nigeriaDate.innerHTML = currentTime.format("MMMM Do YYYY");
+    nigeriaTime.innerHTML = currentTime.format("h:m:s [<small>]A[</small>]");
+  }
 
-  const TimeZone = moment().tz("America/Los_Angeles");
-  losAngelesDate.innerHTML = TimeZone.format("ddd MMM d YYYY");
-  losAngelesTime.innerHTML = TimeZone.format("h:m:s [<small>]A[</small>]");
+  const losAngelesCity = document.getElementById("los-angeles-city");
+  if (losAngelesCity) {
+    const losAngelesDate = document.getElementById("los-angeles-date");
 
-  const turkeyDate = document.getElementById("turkey-date");
-  const turkeyTime = document.getElementById("turkey-time");
+    const losAngelesTime = document.getElementById("los-angeles-time");
 
-  const turkeyTimeZone = moment().tz("Asia/Istanbul");
-  turkeyDate.innerHTML = turkeyTimeZone.format("ddd MMM d YYYY");
-  turkeyTime.innerHTML = turkeyTimeZone.format("h:m:s [<small>]A[</small>]");
+    const TimeZone = moment().tz("America/Los_Angeles");
+    losAngelesDate.innerHTML = TimeZone.format("MMMM Do YYYY");
+    losAngelesTime.innerHTML = TimeZone.format("h:m:s [<small>]A[</small>]");
+  }
+
+  const turkeyCity = document.getElementById("turkey-city");
+  if (turkeyCity) {
+    const turkeyDate = document.getElementById("turkey-date");
+
+    const turkeyTime = document.getElementById("turkey-time");
+
+    const turkeyTimeZone = moment().tz("Asia/Istanbul");
+    turkeyDate.innerHTML = turkeyTimeZone.format("MMMM	Do YYYY");
+    turkeyTime.innerHTML = turkeyTimeZone.format("h:m:s [<small>]A[</small>]");
+  }
+}
+
+function updatedCity(event) {
+  const cityEvent = event.target.value;
+  const cityName = cityEvent.replace("_", " ").split("/")[1];
+  const cityElement = moment.tz(cityEvent);
+  const cityContainer = document.getElementById("cities");
+  cityContainer.innerHTML = `
+  <div class="city">
+          <div>
+            <h2 class="city-content">${cityName} </h2>
+            <div class="city-content date" >${cityElement.format(
+              "MMMM	Do YYYY"
+            )}              
+            </div>
+          </div>
+          <div class="time" id="nigeria-time">${cityElement.format(
+            "h:m:ss"
+          )}<small>${cityElement.format("A")}</small></div>
+        </div>
+ `;
 }
 
 upDated();
 setInterval(upDated, 1000);
+
+const cityInput = document.getElementById("city-input");
+cityInput.addEventListener("change", updatedCity);
